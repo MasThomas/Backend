c09984482508930807c1003b52036f4f3a393af9
Thomas <toma.mas@gmail.com>
2021-11-02 17:41:47 +0100

    modifying firstAdmin path

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const db = require("./models/index")
 db.sequelize.sync().then(function () {
-  require("./conf/firstadmin");
+  require("./config/firstadmin");
 })
 
 app.use(helmet());
