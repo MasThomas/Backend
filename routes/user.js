d281713a50176e3c6e54a8f4943e148793cedb3c
Thomas <toma.mas@gmail.com>
2021-11-29 20:42:51 +0100

    new middleware, new modify routes

--- routes/user.js
+++ routes/user.js
@@ -5,14 +5,20 @@ const userCtrl = require("../controllers/user");
 
 const pwValidator = require('../middleware/pwValidator');
 const mailRegex = require('../middleware/mailRegex');
+const hasRightsToModify = require('../middleware/hasRights')
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config')
 
 router.post("/signup", pwValidator, mailRegex, userCtrl.signup);
 router.post("/login", userCtrl.login);
 router.get("/profils", auth.signin, userCtrl.getAllUsers);
-router.get("/profils/", auth.signin, userCtrl.getOneUser);
-router.put("/profils/:id", auth.signin, multer ,userCtrl.modifyAccount);
+router.get("/profils/:username", auth.signin, userCtrl.getOneUser);
+
+router.put("/profils/profilepicture/:id", auth.signin, hasRightsToModify , multer , userCtrl.modifyAccountProfilePicture);
+router.put("/profils/username/:id", auth.signin, hasRightsToModify , userCtrl.modifyAccountUsername);
+router.put("/profils/email/:id", auth.signin, hasRightsToModify , mailRegex , userCtrl.modifyAccountEmail);
+router.put("/profils/password/:id", auth.signin, hasRightsToModify , pwValidator , userCtrl.modifyAccountPassword);
+
 router.delete("/profils/:id", auth.signin, multer ,userCtrl.deleteAccount);
 
 module.exports = router;
